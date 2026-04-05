24d1bee57911ae1aaff732d19ac53d2959ab5b1a
Md Safkatul Islam <mdsafkatul.stu24@gmail.com>
2026-04-04 20:44:23 -0700

    add tuples demonstration script in Python

---
+++ day_6/tuples.py
@@ -0,0 +1,17 @@
+# syntax
+empty_tuple = ()
+# or using the tuple constructor
+empty_tuple = tuple()
+
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+first_fruit = fruits[0]
+second_fruit = fruits[1]
+last_index =len(fruits) - 1
+last_fruit = fruits[last_index]
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+first_fruit = fruits[0]
+second_fruit = fruits[1]
+last_index =len(fruits) - 1
+last_fruit = fruits[last_index]
